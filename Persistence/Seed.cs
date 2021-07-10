aa1653ecfd14fe9fe5c592de94a6d05e0f4cc963
ihsanbasoeky <mihsanbasuki@gmail.com>
2021-07-10 14:20:12 +0700

    End of section 2

---
+++ Persistence/Seed.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Persistence
+{
+    public class Seed
+    {
+        public static async Task SeedData(DataContext context) {
+            if (context.Activity.Any()) return;
+
+            var activities = new List<Activity>
+            {
+                new Activity
+                {
+                    Title = "Past Activity 1",
+                    Date = DateTime.Now.AddMonths(-2),
+                    Description = "Activity 2 months ago",
+                    Category = "drinks",
+                    City = "London",
+                    Venue = "Pub",
+                },
+                new Activity
+                {
+                    Title = "Past Activity 2",
+                    Date = DateTime.Now.AddMonths(-1),
+                    Description = "Activtiy 1 month ago",
+                    Category = "culture",
+                    City = "Paris",
+                    Venue = "Louvre",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 1",
+                    Date = DateTime.Now.AddMonths(1),
+                    Description = "Activity 1 month in the future",
+                    Category = "culture",
+                    City = "London",
+                    Venue = "National History Museum",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 2",
+                    Date = DateTime.Now.AddMonths(2),
+                    Description = "Activity 2 months in the future",
+                    Category = "music",
+                    City = "London",
+                    Venue = "02 Arena",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 3",
+                    Date = DateTime.Now.AddMonths(3),
+                    Description = "Activity 3 months in the future",
+                    Category = "drinks",
+                    City = "London",
+                    Venue = "Another pub",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 4",
+                    Date = DateTime.Now.AddMonths(4),
+                    Description = "Activity 4 months in the future",
+                    Category = "drinks",
+                    City = "London",
+                    Venue = "Yet another pub",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 5",
+                    Date = DateTime.Now.AddMonths(5),
+                    Description = "Activity 5 months in the future",
+                    Category = "drinks",
+                    City = "London",
+                    Venue = "Also another pub",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 6",
+                    Date = DateTime.Now.AddMonths(6),
+                    Description = "Activity 6 months in the future",
+                    Category = "music",
+                    City = "London",
+                    Venue = "Roundhouse Camden",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 7",
+                    Date = DateTime.Now.AddMonths(7),
+                    Description = "Activity 2 months ago",
+                    Category = "travel",
+                    City = "London",
+                    Venue = "Somewhere on the Thames",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 8",
+                    Date = DateTime.Now.AddMonths(8),
+                    Description = "Activity 8 months in the future",
+                    Category = "film",
+                    City = "London",
+                    Venue = "Cinema",
+                }
+            };
+
+            await context.Activity.AddRangeAsync(activities);
+            await context.SaveChangesAsync();
+        }
+    }
+}
